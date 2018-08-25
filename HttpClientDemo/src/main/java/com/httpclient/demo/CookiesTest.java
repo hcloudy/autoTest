991195c0ae7039427dbdd246a7f12b040bfa7584
hcloudy <370284221@qq.com>
2018-08-25 17:03:26 +0800

    java-testng接口测试

--- HttpClientDemo/src/main/java/com/httpclient/demo/CookiesTest.java
+++ HttpClientDemo/src/main/java/com/httpclient/demo/CookiesTest.java
@@ -1,10 +1,8 @@
 package com.httpclient.demo;
 
-import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.CookieStore;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -13,7 +11,6 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 import java.util.ResourceBundle;
 
